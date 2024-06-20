c711a9901f3591ed74db817151a238b9e4bd482a
Raj Gupta <rajbhkseries@gmail.com>
2024-06-20 16:21:20 +0530

    done .

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ const Header = () => {
   return (
     <div className="relative flex items-center justify-center px-4 py-2 bg-darkb">
       <div className="absolute left-0 flex items-center space-x-4">
-        <button className="text-white font-bold">Exit</button>
-        <button className="text-white font-bold">Save</button>
-        <div className="underline text-white">Last Saved {currentDate}</div>
+       <button className="text-white font-bold border-2 border-white rounded-md hover:bg-black hover:text-amber-600 min-w-[20%] ">Exit</button>
+        <button className="text-white font-bold border-2 border-white rounded-md hover:bg-black hover:text-amber-600 min-w-[20%]">Save</button>
+        <span className="underline text-white font-semibold min-w-[30%] ml-2">Last Saved </span><span className='text-semibold text-blue-300'>{currentDate}</span>
       </div>
       <h1 className="text-lg text-white font-bold">New Email</h1>
     </div>

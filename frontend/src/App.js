50e7781a2a2fa1f65118dcc0be0ad0fa83280839
santa116-1 <totoplife116@gmail.com>
2024-06-23 02:37:45 +0900

    add menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,57 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+// import "./App.css";
+// import Success from "./pages/Success";
+// import Authorization from "./components/Authorization";
+// import SendSchedulePage from "./pages/SendSchedulePage";
+// import Edit from "./pages/Edit";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Navigate to="/athorization" />}  />
+//         <Route path="/athorization" element={<Authorization/>} />
+//         <Route path="/success" element={<Success/>} />
+//         <Route path="/send-schedule" element={<SendSchedulePage/>}  />
+//         <Route path="/edit" element={<Edit/>}  />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/SendSchedulePage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import MenuBar from "./components/MenuBar";
+import Header from "./components/Header";
 import Success from "./pages/Success";
 import Authorization from "./components/Authorization";
 import SendSchedulePage from "./pages/SendSchedulePage";
+import Edit from "./pages/Edit";
+import SendSchedule from "./data/SendSchedule";
+import "./App.css";
 
-const App = () => {
+function App() {
   return (
     <Router>
+      <Header />
+      <MenuBar />
       <Routes>
-        <Route path="/" element={<Navigate to="/athorization" />}  />
-        <Route path="/athorization" element={<Authorization/>} />
-        <Route path="/success" element={<Success/>} />
-        <Route path="/send-schedule" element={<SendSchedulePage/>}  />
+        <Route path="/" element={<Navigate to="/athorization" />} />
+        <Route path="/athorization" element={<Authorization />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/send-schedule" element={<SendSchedule />} />
+        <Route path="/edit" element={<Edit />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
--- frontend/src/components/MenuBar.js
+++ frontend/src/components/MenuBar.js
@@ -1,30 +1,78 @@
 import React, { useState } from "react";
 import { PiFlask } from "react-icons/pi";
 import HoverInfo from "../components/HoverInfo";
+import { Link, useNavigate, NavLink } from "react-router-dom";
+import SendSchedulePage from "../pages/SendSchedulePage";
 
 const MenuBar = () => {
   const [activeTab, setActiveTab] = useState("Send or Schedule");
-  const tabs = ["Edit",  "Settings", "Send or Schedule", "Automation"];
+  const tabs = ["Edit", "Settings", "Send or Schedule", "Automation"];
+  const navigate = useNavigate();
+  const EditClick = () => {
+    navigate("/edit");
+  };
 
   return (
-    <div className="flex justify-between items-center px-10 border-b-2 border-gray-200">
+    <div className="flex justify-between items-center px-10 py-3 border-b-2 border-gray-200">
       <div className="flex flex-wrap items-center">
         <PiFlask style={{ color: "#0091AE", fontSize: "1.5em" }} />
         <h3 className="text-[#0091AE] font-bold ">Create A/B test</h3>
         <HoverInfo text=" A/B testing compares two webpage versions to see which performs better in driving SEO and user engagement." />
       </div>
       <div className="flex justify-center border-gray-200">
-        {tabs.map((tab) => (
-          <button
-            key={tab}
-            className={`py-4 px-8 mb-0 font-semibold text-md h-[100%] ${
-              activeTab === tab ? "border-b-4 border-darkb text-darkb" : ""
-            }`}
-            onClick={() => setActiveTab(tab)}
-          >
-            {tab}
-          </button>
-        ))}
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <NavLink
+                to="/edit"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "py-4 px-8 mb-0 font-semibold text-md h-[100%] border-b-4 border-gray-600 text-darkb"
+                    : "py-4 px-8 mb-0 font-semibold text-md h-[100%]"
+                }
+              >
+                Edit
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/success"
+                className={({ isActive }) =>
+                    isActive
+                    ? "py-4 px-8 mb-0 font-semibold text-md h-[100%] border-b-4 border-gray-600 text-darkb"
+                    : "py-4 px-8 mb-0 font-semibold text-md h-[100%]"
+                }
+              >
+                Settings
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/send-schedule"
+                className={({ isActive }) =>
+                    isActive
+                    ? "py-4 px-8 mb-0 font-semibold text-md h-[100%] border-b-4 border-gray-600 text-darkb"
+                    : "py-4 px-8 mb-0 font-semibold text-md h-[100%]"
+                }
+              >
+                Send or Schedule
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/automation"
+                className={({ isActive }) =>
+                    isActive
+                    ? "py-4 px-8 mb-0 font-semibold text-md h-[100%] border-b-4 border-gray-600 text-darkb"
+                    : "py-4 px-8 mb-0 font-semibold text-md h-[100%]"
+                }
+              >
+                Automation
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
       <div>
         <button className="min-w-[8%] h-[100%] mr-5 p-2 border-[1.5px] border-[#CBD6E2] bg-[#EAF0F5] rounded-sm text-[#4F6E91] text-sm">

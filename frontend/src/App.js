b4208a9efdf60554959f3b1ee229acad3dfdb991
santa116-1 <totoplife116@gmail.com>
2024-06-22 23:23:21 +0900

    fix router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import SendSchedule from "./pages/SendSchedule";
 import Home from "./pages/Home";
 import Success from "./pages/Success";
 import MailSetting from "./components/MailSetting";
+import Authorization from "./components/Authorization";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Navigate to="/athorization" />}  />
+        <Route path="/athorization" element={<Authorization/>} />
         <Route path="/success" element={<Success/>} />
         <Route path="/mail-setting" element={<MailSetting/>} />
         <Route path="/send-schedule" element={<SendSchedule/>} />

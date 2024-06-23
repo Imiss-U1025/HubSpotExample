241829a08da55e5fa01c29ffe4706411391257fa
santa116-1 <totoplife116@gmail.com>
2024-06-23 23:02:44 +0900

    add force

--- frontend/src/pages/Edit.js
+++
@@ -1,9 +0,0 @@
-import SendSchedule from "./SendSchedule";
-import MenuBar from "../components/MenuBar";
-import Header from "../components/Header";
-
-const Edit = () => {
-  return <>Edit content</>;
-};
-
-export default Edit;
